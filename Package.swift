20e1814bd6ada09c872d1a6b040ea72288269b67
Daniel Watson <d.m.watson1985@gmail.com>
2024-02-05 17:17:08 +0100

    // Omitted optional parameters that were previously placeholders

--- Package.swift
+++ Package.swift
@@ -1,11 +1,10 @@
-// swift-tools-version: 5.9
-
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "MiseboxUseriOS",
     platforms: [
-        .iOS(.v16) // Specify iOS 16 as the minimum deployment target
+        .iOS(.v16)
     ],
     products: [
         .library(
@@ -13,19 +12,14 @@ let package = Package(
             targets: ["MiseboxUseriOS"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ddddeano/FirebaseiOSMisebox.git", from: "1.0.0"),
-        // Add other package dependencies here if needed
+        .package(url: "https://github.com/ddddeano/FirebaseiOSMisebox.git", from: "1.0.0")
     ],
     targets: [
         .target(
             name: "MiseboxUseriOS",
             dependencies: [
                 "FirebaseiOSMisebox"
-            ],
-            path: "Sources/MiseboxUseriOS", // Optional: Custom path
-            exclude: ["ExcludedFile.swift"], // Optional: Exclude files
-            sources: ["IncludeThisFolderOrFile"], // Optional: Specific sources
-            resources: [.process("Resources")] // Optional: Resources
+            ]
         ),
         .testTarget(
             name: "MiseboxUseriOSTests",

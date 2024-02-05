f0c4688b9cc54377d0b747fc29943f126020bc0d
Daniel Watson <d.m.watson1985@gmail.com>
2024-02-05 15:58:39 +0100

    package updated

--- Package.swift
+++ Package.swift
@@ -1,21 +1,34 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import PackageDescription
+// swift-tools-version:5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import PackageDescription
+//
 let package = Package(
     name: "MiseboxUseriOS",
+    platforms: [
+        .iOS(.v16) // Set the iOS version you want to support
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MiseboxUseriOS",
             targets: ["MiseboxUseriOS"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/ddddeano/FirebaseiOSMisebox.git", from: "1.0.0"), // Replace with the correct version
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MiseboxUseriOS"),
+            name: "MiseboxUseriOS",
+            dependencies: [
+                "FirebaseiOSMisebox", // Add the dependency here
+            ]
+        ),
         .testTarget(
             name: "MiseboxUseriOSTests",
             dependencies: ["MiseboxUseriOS"]),

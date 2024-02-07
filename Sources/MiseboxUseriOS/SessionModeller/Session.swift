3a41b02435f7a003f6863e32988247defe8e9b20
Daniel Watson Machine 2 <d.m.watson1985gmail.com>
2024-02-07 09:37:28 +0100

    public init(role: MiseboxUserManager.Role) {             self.role = role         }

--- Sources/MiseboxUseriOS/SessionModeller/Session.swift
+++ Sources/MiseboxUseriOS/SessionModeller/Session.swift
@@ -12,9 +12,13 @@ import Combine
 extension SessionManager {
     
     public class Session: ObservableObject {
-        public let role: MiseboxUserManager.Role = .miseboxUser
+        public let role: MiseboxUserManager.Role
         @Published public var id = ""
         @Published public var isDarkMode = false
+        
+        public init(role: MiseboxUserManager.Role) {
+            self.role = role
+        }
     }
 }
 

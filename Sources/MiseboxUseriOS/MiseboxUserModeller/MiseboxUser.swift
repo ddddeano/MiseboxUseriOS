e3bdd99da7ff1d4bd24a5868338e10dce94c456a
Daniel Watson <d.m.watson1985@gmail.com>
2024-02-13 19:04:46 +0100

    miseCODE = data ["misecode"] as? String ?? ""

--- Sources/MiseboxUseriOS/MiseboxUserModeller/MiseboxUser.swift
+++ Sources/MiseboxUseriOS/MiseboxUserModeller/MiseboxUser.swift
@@ -18,7 +18,9 @@ extension MiseboxUserManager {
         public let collection = "misebox-users"
         
         @Published public var id: String = ""
+        
         @Published public var username: String = ""
+        @Published public var miseCODE: String = ""
         @Published public var email: String = ""
         @Published public var imageUrl: String = defaultImage
         @Published public var verified: Bool = false
@@ -38,10 +40,11 @@ extension MiseboxUserManager {
             self.id = documentSnapshot.documentID
             update(with: data)
         }
-
+        
         public func update(with data: [String: Any]) {
             username = data["username"] as? String ?? ""
-            username = data["email"] as? String ?? ""
+            email = data["email"] as? String ?? ""
+            miseCODE = data ["misecode"] as? String ?? ""
             imageUrl = data["image_url"] as? String ?? defaultImage
             verified = data["verified"] as? Bool ?? false
             
@@ -54,18 +57,21 @@ extension MiseboxUserManager {
             return [
                 "username": username,
                 "email": email,
+                "misecode": miseCODE,
                 "image_url": imageUrl,
                 "verified": verified,
                 "user_roles": userRoles.map { $0.toFirestore() }
             ]
         }
         public func resetFields() {
-               id = ""
-               username = ""
-               imageUrl = defaultImage
-               verified = false
-               userRoles = []
-           }
+            id = ""
+            username = ""
+            email = ""
+            miseCODE = ""
+            imageUrl = defaultImage
+            verified = false
+            userRoles = []
+        }
     }
 }
 

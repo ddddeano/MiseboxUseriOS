55f2a111e3a159f3f3a28604925455b4340ee8c7
Daniel Watson <d.m.watson1985@gmail.com>
2024-02-13 11:01:40 +0100

    username = data["email"] as? String ?? ""

--- Sources/MiseboxUseriOS/MiseboxUserModeller/MiseboxUser.swift
+++ Sources/MiseboxUseriOS/MiseboxUserModeller/MiseboxUser.swift
@@ -19,6 +19,7 @@ extension MiseboxUserManager {
         
         @Published public var id: String = ""
         @Published public var username: String = ""
+        @Published public var email: String = ""
         @Published public var imageUrl: String = defaultImage
         @Published public var verified: Bool = false
         @Published public var userRoles: [UserRole] = []
@@ -35,39 +36,29 @@ extension MiseboxUserManager {
         public init?(documentSnapshot: DocumentSnapshot) {
             guard let data = documentSnapshot.data() else { return nil }
             self.id = documentSnapshot.documentID
-            print("Initializing MiseboxUser with ID: \(self.id)")
-            print("Username: \(data["username"] as? String ?? "N/A")")
-            print("Image URL: \(data["image_url"] as? String ?? "N/A")")
-            print("Verified: \(data["verified"] as? Bool ?? false)")
             update(with: data)
         }
 
         public func update(with data: [String: Any]) {
             username = data["username"] as? String ?? ""
+            username = data["email"] as? String ?? ""
             imageUrl = data["image_url"] as? String ?? defaultImage
             verified = data["verified"] as? Bool ?? false
             
             if let rolesData = data["user_roles"] as? [[String: Any]] {
                 userRoles = rolesData.compactMap(UserRole.init)
             }
-
         }
         
         public func toFirestore() -> [String: Any] {
             return [
                 "username": username,
+                "email": email,
                 "image_url": imageUrl,
                 "verified": verified,
                 "user_roles": userRoles.map { $0.toFirestore() }
             ]
         }
-        public func resetFields() {
-               id = ""
-               username = ""
-               imageUrl = defaultImage
-               verified = false
-               userRoles = []
-           }
     }
 }
 
--- Sources/MiseboxUseriOS/MiseboxUserModeller/MiseboxuserFunctions.swift
+++ Sources/MiseboxUseriOS/MiseboxUserModeller/MiseboxuserFunctions.swift
@@ -13,38 +13,25 @@ import FirebaseiOSMisebox
 extension MiseboxUserManager {
     
     public func checkMiseboxUserExistsInFirestore() async throws -> Bool {
-        print("Checking if Misebox user exists in Firestore... Collection: '\(miseboxUser.collection)', Document ID: '\(self.id)'")
-
         guard !self.id.isEmpty else {
-            print("Document ID is empty. Cannot proceed with checking document existence in Firestore.")
             return false
         }
-
         let exists = try await firestoreManager.checkDocumentExists(collection: miseboxUser.collection, documentID: self.id)
-        print("Existence check completed. Document exists: \(exists)")
         return exists
     }
 
-
     public func primeMiseboxUser(id: String) {
-        print("Before priming Misebox user, current user ID: \(self.miseboxUser.id)")
-        print("Priming Misebox user with session ID: \(id)...")
         self.miseboxUser.prime(id: id)
        /* not sure what to do with image yet we can wit if self.imageUrl.isEmpty {
             self.miseboxUser.imageUrl = defaultImage
         }*/
-        print("After priming Misebox user, new user ID: \(self.miseboxUser.id)")
     }
 
     public func primeMiseboxUserProfile(id: String) {
-        print("Before priming Misebox user profile, current profile ID: \(self.miseboxUserProfile.id)")
-        print("Priming Misebox user profile with user ID: \(id)...")
         self.miseboxUserProfile.prime(id: id)
-        print("After priming Misebox user profile, new profile ID: \(self.miseboxUserProfile.id)")
     }
     
     public func setMiseboxUserAndProfile() async throws {
-        print("Setting Misebox user and profile in Firestore...")
         try await firestoreManager.setDoc(entity: self.miseboxUser)
         try await firestoreManager.setDoc(entity: self.miseboxUserProfile)
     }

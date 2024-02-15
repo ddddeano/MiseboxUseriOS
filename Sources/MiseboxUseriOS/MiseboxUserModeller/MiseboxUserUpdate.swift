be4865bcbbc44acd52810c13d68fff0d0127a16a
Daniel Watson <d.m.watson1985@gmail.com>
2024-02-15 21:29:29 +0100

    func updateMiseboxUser() {         Task {             do {                 await firestoreManager.updateDocument(for: self.miseboxUser)             }         }     }

--- Sources/MiseboxUseriOS/MiseboxUserModeller/MiseboxUserUpdate.swift
+++ Sources/MiseboxUseriOS/MiseboxUserModeller/MiseboxUserUpdate.swift
@@ -2,12 +2,66 @@ import Foundation
 import FirebaseiOSMisebox
 
 extension MiseboxUserManager {
+    
+    func updateMiseboxUser() {
+        Task {
+            do {
+                await firestoreManager.updateDocument(for: self.miseboxUser)
+            }
+        }
+    }
 
-    public func updateMiseboxUser<T: Updatable>(update: T) async throws {
-        try await firestoreUpdateManager.updateDocument(for: self.miseboxUser, with: update)
+    func updateMiseboxUserProfile() {
+        Task {
+            do {
+                await firestoreManager.updateDocument(for: self.miseboxUserProfile)
+            }
+        }
+    }
+
+    
+    enum UpdateContext {
+        case username
+        case miseCODE
+        case email
+        case imageUrl
+        case verified
+        case userRoles
+        case fullName
+        case subscription
+        case accountProviders
     }
     
-    public func updateMiseboxUserProfile<T: Updatable>(update: T) async throws {
-        try await firestoreUpdateManager.updateDocument(for: self.miseboxUserProfile, with: update)
+    func update(context: UpdateContext) async {
+        switch context {
+        case .username:
+            print("Current username: \(self.miseboxUser.username)")
+            updateMiseboxUser()
+        case .miseCODE:
+            print("Current miseCode: \(self.miseboxUser.miseCODE)")
+            updateMiseboxUser()
+        case .email:
+            print("Current email: \(self.miseboxUser.email)")
+            updateMiseboxUser()
+        case .imageUrl:
+            print("Current imageUrl: \(self.miseboxUser.imageUrl)")
+            updateMiseboxUser()
+        case .verified:
+            print("Current verified status: \(self.miseboxUser.verified)")
+            updateMiseboxUser()
+        case .userRoles:
+            print("Current userRoles: \(self.miseboxUser.userRoles)")
+            updateMiseboxUser()
+        case .fullName:
+            print("Current fullName: \(self.miseboxUserProfile.fullName)")
+            updateMiseboxUserProfile()
+        case .subscription:
+            print("Current subscription: \(self.miseboxUserProfile.subscription)")
+            updateMiseboxUserProfile()
+        case .accountProviders:
+            print("Current accountProviders: \(self.miseboxUserProfile.accountProviders)")
+            updateMiseboxUserProfile()
+        }
     }
 }
+

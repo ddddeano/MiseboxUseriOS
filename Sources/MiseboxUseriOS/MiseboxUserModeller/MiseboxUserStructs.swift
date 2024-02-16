b3a648e9b22d1a3db1fb622220f19b2ac1b481b3
Daniel Watson <d.m.watson1985@gmail.com>
2024-02-16 20:58:35 +0100

    public static let miseboxUser = Role(doc: "misebox-user", collection: "misebox-users", color: .gray, isAvailable: true)

--- Sources/MiseboxUseriOS/MiseboxUserModeller/MiseboxUserStructs.swift
+++ Sources/MiseboxUseriOS/MiseboxUserModeller/MiseboxUserStructs.swift
@@ -10,20 +10,18 @@ import FirebaseiOSMisebox
 import SwiftUI
 
 extension MiseboxUserManager {
-    
     public struct Role {
         public let doc: String
         public let collection: String
         public let color: Color
         public let isAvailable: Bool
         
-        // Define roles without the 'motto' property
+        // Define roles without the 'mottoKey' property
         public static let miseboxUser = Role(doc: "misebox-user", collection: "misebox-users", color: .gray, isAvailable: true)
         public static let chef = Role(doc: "chef", collection: "chefs", color: .green, isAvailable: true)
         public static let agent = Role(doc: "agent", collection: "agents", color: .blue, isAvailable: true)
         public static let recruiter = Role(doc: "recruiter", collection: "recruiters", color: .orange, isAvailable: true)
         
-        // Array of all role cases for easy iteration or finding
         public static let allCases: [Role] = [.miseboxUser, .chef, .agent, .recruiter]
         
         // Helper method to find a role by its document identifier

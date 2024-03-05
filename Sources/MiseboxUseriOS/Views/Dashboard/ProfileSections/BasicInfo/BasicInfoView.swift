111d198c227bdd2e7fb54869f49c2adf8af12532
Daniel Watson <d.m.watson1985@gmail.com>
2024-03-05 18:54:24 +0100

    removed handle basic infor for testing

--- Sources/MiseboxUseriOS/Views/Dashboard/ProfileSections/BasicInfo/BasicInfoView.swift
+++ Sources/MiseboxUseriOS/Views/Dashboard/ProfileSections/BasicInfo/BasicInfoView.swift
@@ -9,25 +9,3 @@ import Foundation
 import SwiftUI
 import MiseboxiOSGlobal
 
-public struct BasicInfoView: View {
-    @EnvironmentObject var miseboxUser: MiseboxUserManager.MiseboxUser
-    @EnvironmentObject var miseboxUserProfile: MiseboxUserManager.MiseboxUserProfile
-    public init() {
-    }
-    
-    public var body: some View {
-        VStack(alignment: .leading, spacing: 15) {
-            Divider()
-                .padding(.top, 100)
-            SectionTitle(title: "Basic Information")
-            Divider()
-            // HandleProfileView(vm: vm, handle: $miseboxUser.handle)
-            // FullNameProfileView(vm: vm, fullName: $miseboxUserProfile.fullName)
-            // EmailProfileView(vm: vm, email: $miseboxUser.email)
-            Divider()
-            Spacer()
-        }
-        .padding()
-        .sheetStyle(backgroundColor: .purple.opacity(0.1))
-    }
-}
